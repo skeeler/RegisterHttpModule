e888a2f20b4addb8db4d8f341a8e6a217389336f
Steve Keeler <github@cairngrove.ca>
2018-06-21 12:12:21 -0400

    initial create (credit: David Ebbo)

---
+++ TestModule/MyModule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RegisterHttpModule.TestModule {
+    public class PreApplicationStartCode {
+        public static void Start() {
+            // Register our module
+            Microsoft.Web.Infrastructure.DynamicModuleHelper.DynamicModuleUtility.RegisterModule(typeof(MyModule));
+        }
+    }
+    
+    public class MyModule: IHttpModule {
+        public void Init(HttpApplication application) {
+            application.BeginRequest += new EventHandler(OnBeginRequest);
+        }
+
+        void OnBeginRequest(object sender, EventArgs e) {
+            ((HttpApplication)sender).Response.Write("MyModule.OnBeginRequest<br>");
+        }
+
+        public void Dispose() {
+        }
+    }
+}
